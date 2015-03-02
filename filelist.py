77948cb6097dcb91bbd6b5081370430a00bb6079
Jonny Lamb <jonnylamb@jonnylamb.com>
2015-03-02 20:07:07 +0100

    filelist: add another stupid filter for directories to use

--- filelist.py
+++ filelist.py
@@ -4,7 +4,7 @@ import json
 def main():
     results = {}
 
-    for f in [x for x in os.listdir('.') if os.path.isdir(x)]:
+    for f in [x for x in os.listdir('.') if os.path.isdir(x) and len(x.split('-')) == 3]:
         results[f] = []
         for t in [x for x in os.listdir(f) if x.endswith('.json')]:
             results[f].append(t)
